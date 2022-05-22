1a333a274f617d84ebd45bdc18018218b8d92828
minho0315 <dlalsgh369@naver.com>
2022-05-22 23:14:55 +0900

    기본 키 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,13 +17,23 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member1 = new Member();
+            member1.setUsername("A");
 
-            Member member = new Member();
-            member.setId(3L);
-            member.setUsername("C");
-            member.setRoleType(RoleType.GUEST);
+            Member member2 = new Member();
+            member2.setUsername("B");
 
-            em.persist(member);
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+
+            System.out.println("==================");
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("==================");
 
             tx.commit();
         } catch (Exception e) {
